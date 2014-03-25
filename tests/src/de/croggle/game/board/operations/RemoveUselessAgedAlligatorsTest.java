af46767a86a9bc4523ae643c2b40ddb2fed9d555
Lukas Boehm <suluke93@gmail.com>
2014-03-25 21:45:34 +0100

    Add testcase for RemoveUselessAgedAlligators

---
+++ tests/src/de/croggle/game/board/operations/RemoveUselessAgedAlligatorsTest.java
@@ -0,0 +1,44 @@
+package de.croggle.game.board.operations;
+
+import junit.framework.TestCase;
+import de.croggle.game.board.Board;
+import de.croggle.util.convert.AlligatorToLambda;
+import de.croggle.util.convert.LambdaToAlligator;
+
+public class RemoveUselessAgedAlligatorsTest extends TestCase {
+	public void testLeftmost() {
+		// λ
+		String term = "(x x)";
+		Board b = LambdaToAlligator.convert(term);
+		RemoveUselessAgedAlligators.remove(b, null);
+		assertEquals("x x", AlligatorToLambda.convert(b));
+	}
+
+	public void testMiddle() {
+		String term = "x (x x) x";
+		Board b = LambdaToAlligator.convert(term);
+		RemoveUselessAgedAlligators.remove(b, null);
+		assertEquals("x x x x", AlligatorToLambda.convert(b));
+	}
+
+	public void testAbstracted() {
+		String term = "λx.(x x)";
+		Board b = LambdaToAlligator.convert(term);
+		RemoveUselessAgedAlligators.remove(b, null);
+		assertEquals("λx.x x", AlligatorToLambda.convert(b));
+	}
+
+	public void testNoChange() {
+		String term = "λx.x (x x)";
+		Board b = LambdaToAlligator.convert(term);
+		RemoveUselessAgedAlligators.remove(b, null);
+		assertEquals("λx.x (x x)", AlligatorToLambda.convert(b));
+	}
+
+	public void testCase0() {
+		String term = "λx.y (x x)";
+		Board b = LambdaToAlligator.convert(term);
+		RemoveUselessAgedAlligators.remove(b, null);
+		assertEquals("λx.y x x", AlligatorToLambda.convert(b));
+	}
+}
