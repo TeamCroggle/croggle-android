3a3ecdf3d346cf352a3f0f43f4e8a727323ae284
Lukas Boehm <suluke93@gmail.com>
2014-05-11 00:04:02 +0200

    Adapt tests to renaming changes

--- tests/src/de/croggle/game/board/operations/RemoveNeedlessAgedAlligatorsTest.java
+++ tests/src/de/croggle/game/board/operations/RemoveNeedlessAgedAlligatorsTest.java
@@ -5,40 +5,40 @@ import de.croggle.game.board.Board;
 import de.croggle.util.convert.AlligatorToLambda;
 import de.croggle.util.convert.LambdaToAlligator;
 
-public class RemoveUselessAgedAlligatorsTest extends TestCase {
+public class RemoveNeedlessAgedAlligatorsTest extends TestCase {
 	public void testLeftmost() {
 		// λ
 		String term = "(x x)";
 		Board b = LambdaToAlligator.convert(term);
-		RemoveUselessAgedAlligators.remove(b, null);
+		RemoveNeedlessAgedAlligators.remove(b, null);
 		assertEquals("x x", AlligatorToLambda.convert(b));
 	}
 
 	public void testMiddle() {
 		String term = "x (x x) x";
 		Board b = LambdaToAlligator.convert(term);
-		RemoveUselessAgedAlligators.remove(b, null);
+		RemoveNeedlessAgedAlligators.remove(b, null);
 		assertEquals("x x x x", AlligatorToLambda.convert(b));
 	}
 
 	public void testAbstracted() {
 		String term = "λx.(x x)";
 		Board b = LambdaToAlligator.convert(term);
-		RemoveUselessAgedAlligators.remove(b, null);
+		RemoveNeedlessAgedAlligators.remove(b, null);
 		assertEquals("λx.x x", AlligatorToLambda.convert(b));
 	}
 
 	public void testNoChange() {
 		String term = "λx.x (x x)";
 		Board b = LambdaToAlligator.convert(term);
-		RemoveUselessAgedAlligators.remove(b, null);
+		RemoveNeedlessAgedAlligators.remove(b, null);
 		assertEquals("λx.x (x x)", AlligatorToLambda.convert(b));
 	}
 
 	public void testCase0() {
 		String term = "λx.y (x x)";
 		Board b = LambdaToAlligator.convert(term);
-		RemoveUselessAgedAlligators.remove(b, null);
+		RemoveNeedlessAgedAlligators.remove(b, null);
 		assertEquals("λx.y x x", AlligatorToLambda.convert(b));
 	}
 }
